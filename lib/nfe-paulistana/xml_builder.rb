be2ba3e667cd48bb641e17c5f7b96e5b83c9ce8e
Rafael Ivan Garcia <rafael.ivan@infosimples.com.br>
2018-11-07 22:28:30 -0200

    Adds valor_carga_tributaria and percentual_carga_tributaria

--- lib/nfe-paulistana/xml_builder.rb
+++ lib/nfe-paulistana/xml_builder.rb
@@ -54,6 +54,8 @@ module NfePaulistana
       :email_tomador => '',
       :email_intermediario => '',
       :discriminacao => '',
+      :valor_carga_tributaria => '',
+      :percentual_carga_tributaria => '',
       :wsdl => 'https://nfe.prefeitura.sp.gov.br/ws/lotenfe.asmx?wsdl'
     }
 
@@ -63,7 +65,7 @@ module NfePaulistana
     end
 
     private
-    
+
     def xml(method, data, certificado)
       builder = Nokogiri::XML::Builder.new do |xml|
         xml.send(METHODS[method], "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance", "xmlns:xsd" => "http://www.w3.org/2001/XMLSchema", "xmlns" => "http://www.prefeitura.sp.gov.br/nfe" ) {
@@ -225,7 +227,7 @@ module NfePaulistana
         xml.AliquotaServicos data[:aliquota_servicos]
         xml.ISSRetido data[:iss_retido]
         unless (data[:cpf_tomador].blank? and data[:cnpj_tomador].blank?)
-          xml.CPFCNPJTomador { 
+          xml.CPFCNPJTomador {
             xml.CPF data[:cpf_tomador] unless data[:cpf_tomador].blank?
             xml.CNPJ data[:cnpj_tomador] unless data[:cnpj_tomador].blank?
           }
@@ -248,7 +250,7 @@ module NfePaulistana
         xml.EmailTomador data[:email_tomador]
 =begin
         unless (data[:cpf_intermediario].blank? and data[:cnpj_intermediario].blank?)
-          xml.CPFCNPJIntermediario { 
+          xml.CPFCNPJIntermediario {
             xml.CPF data[:cpf_intermediario] unless data[:cpf_intermediario].blank?
             xml.CNPJ data[:cnpj_intermediario] unless data[:cnpj_intermediario].blank?
           }
@@ -258,6 +260,8 @@ module NfePaulistana
         end
 =end
         xml.Discriminacao data[:discriminacao]
+        xml.ValorCargaTributaria data[:valor_carga_tributaria]
+        xml.PercentualCargaTributaria data[:percentual_carga_tributaria]
       }
     end
 
@@ -278,7 +282,7 @@ module NfePaulistana
       end
 
       # 3. Add Elements to Signature Node
-      
+
       # 3.1 Create Signature Info
       signature_info = Nokogiri::XML::Node.new('SignedInfo', xml)
 
@@ -313,7 +317,7 @@ module NfePaulistana
       child_node  = Nokogiri::XML::Node.new('DigestMethod', xml)
       child_node['Algorithm'] = 'http://www.w3.org/2000/09/xmldsig#sha1'
       reference.add_child child_node
-      
+
       # 3.6 Add DigestValue
       child_node  = Nokogiri::XML::Node.new('DigestValue', xml)
       child_node.content = xml_digest
@@ -335,7 +339,7 @@ module NfePaulistana
 
       # 5 Create KeyInfo
       key_info = Nokogiri::XML::Node.new('KeyInfo', xml)
-      
+
       # 5.1 Add X509 Data and Certificate
       x509_data = Nokogiri::XML::Node.new('X509Data', xml)
       x509_certificate = Nokogiri::XML::Node.new('X509Certificate', xml)
@@ -360,7 +364,7 @@ module NfePaulistana
       value = part_1 + part_2
       assinatura_simples(value, certificado)
     end
-    
+
     def assinatura_envio_rps(data, certificado)
       part_1 = data[:inscricao_prestador].rjust(8,'0')
       part_2 = data[:serie_rps].ljust(5)
