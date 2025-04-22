c60949bcb70f10367270bcd89b005b80bc4d6349
Rafael Ivan Garcia <rafael.ivan@infosimples.com.br>
2025-04-22 11:12:05 -0300

    Bump version to 1.0.6

--- lib/nfe-paulistana.rb
+++ lib/nfe-paulistana.rb
@@ -11,19 +11,19 @@ module NfePaulistana
   WSDL = 'https://nfe.prefeitura.sp.gov.br/ws/lotenfe.asmx?wsdl'
 
   def self.enviar(data = {})
-    certificado = OpenSSL::PKCS12.new(File.read(data[:cert_path]), data[:cert_pass]) 
+    certificado = OpenSSL::PKCS12.new(File.read(data[:cert_path]), data[:cert_pass])
     client = get_client(certificado)
 
     begin
       response = client.call(:envio_rps, message: {
-        input: [ 
-            "EnvioRPSRequest", 
-              {"xmlns" => "http://www.prefeitura.sp.gov.br/nfe"}
+        input: [
+            "EnvioRPSRequest",
+              { "xmlns" => "http://www.prefeitura.sp.gov.br/nfe" }
         ],
         body: XmlBuilder.new.xml_for(:envio_rps, data, certificado),
         version: 2
       })
-      Response.new(xml: response.hash[:envio_rps_response][:retorno_xml], method: :envio_rps_response)
+      Response.new(xml: response.full_hash[:envio_rps_response][:retorno_xml], method: :envio_rps_response)
     rescue Savon::Error => error
     end
   end
